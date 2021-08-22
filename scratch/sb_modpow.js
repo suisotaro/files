05a413be90de5c89df7746d9e43df33e7b077243
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-08-23 08:29:35 +0900

    Create sb_modpow.js

---
+++ scratch/sb_modpow.js
@@ -0,0 +1,46 @@
+const menuIconURI = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADoAAAA8CAYAAAA34qk1AAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAOqADAAQAAAABAAAAPAAAAAAs615UAAAAHGlET1QAAAACAAAAAAAAAB4AAAAoAAAAHgAAAB4AAAECIb82kwAAAM5JREFUaAXslNEJxDAMQz1td7qdspMvgWsbqCqkzwMFDMF2RP1st6q7LSvtfKauY5rqnTWmvmNlFbmgiMcpcuWKsleaU+TKTaGvnb6Y8ks6OscIQeDYntGM7gSJIGRHs6PPdYEetIfMB0WIE40n82V0M7pknPYQG1MU298qdzamKKZoJicEQiAEQiAEQiAEQiAE/pPAGF2OqVUeR5djqu6Z52ivXKvIBUQ99oeowr88W9/pZgoF3bCJAw3msvXT0ZefFKO8x2zi+2Phbup/AQAA///JunG3AAAArUlEQVTtmFEKhDAMBXNa7+SdvFOXwrqgkO2bv12cQpDW+DCT14JWHcdAUeHYtlEkQtlPGtGeuajICSUd+EVS4Xce1rejjaVT8Jh4KmxHv58Jz9mj0DGmS0ACEpCABCQgAQlI4G8I7PsoEmlhP/eZRoqcuemw0OZfUgrwzMMg7Wizd0+iqysmvhK83cf6dtSO3jzUTLG1Gp1uGetrXa3bmem6jq11fXw5w/oPse4LtniNtZAV2lEAAAAASUVORK5CYII=";
+
+class suisotaroBlocks {
+  getInfo() {
+    return {
+      id: "suisotaroBlocks",
+      name: "suisotaroBlocks",
+      menuIconURI: menuIconURI,
+      blocks: [
+        {
+          opcode: "smodpow",
+          blockType: "reporter",
+          text: " [a] ^ [b] % [c] ",
+          arguments: {
+            a: {
+              type: "number",
+              defaultValue: 0
+            },
+            b: {
+              type: "number",
+              defaultValue: 0
+            },
+            c: {
+              type: "number",
+              defaultValue: 0
+            }
+          }
+        }
+      ]
+    };
+  }
+  
+  smodpow({a, b, c}) {
+    var n=1;
+    while(b>0){
+      if((b&1)==1){
+        n = (n*a)%c
+      }
+      b>>=1;
+      a=(a*a)%c
+    }
+    return n;
+  }
+}
+
+Scratch.extensions.register(new suisotaroBlocks());
