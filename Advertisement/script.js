1b09684a66d5ccab67b8022eb921dc0775644d4a
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-03 18:57:02 +0900

    Update script.js

--- Advertisement/script.js
+++ Advertisement/script.js
@@ -1,6 +1,6 @@
 setInterval(()=>{["ytp-ad-overlay-close-button","ytp-ad-skip-button"].map(e=>Array.from(document.getElementsByClassName(e))).flat().forEach(e=>e.click())},30);
 
 const newStyle = document.createElement("style");
-newStyle.innerText = ".adsbygoogle .ytp-ad-overlay-container{display:none !important;}"; //.adsbygoogle{position:absolute;left:-1000px;top:-1500px;}";
-document.body.[0].appendChild(newStyle);
+newStyle.innerText = ".adsbygoogle,.ytp-ad-overlay-container{display:none !important;}"; //.adsbygoogle{position:absolute;left:-1000px;top:-1500px;}";
+document.body.appendChild(newStyle);
 alert("広告ブロック🚫！\nby suisotaro");
