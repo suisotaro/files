cc9c242a9b7a2b9117d455c4f5399496325b0e1e
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-03 17:44:18 +0900

    Update script.js

--- Advertisement/script.js
+++ Advertisement/script.js
@@ -1,5 +1,5 @@
 setInterval(()=>{["ytp-ad-overlay-close-button","ytp-ad-skip-button"].map(e=>Array.from(document.getElementsByClassName(e))).flat().forEach(e=>e.click())},30);
 
 const newStyle = document.createElement("style");
-newStyle.innerText = ".ytp-ad-overlay-container{display:none!important;}.adsbygoogle{position:absolute;left:-1000px;top:-1500px;}";
+newStyle.innerText = ".adsbygoogle .ytp-ad-overlay-container{display:none!important;}"; //.adsbygoogle{position:absolute;left:-1000px;top:-1500px;}";
 document.getElementsByTagName("head").item(0).appendChild(newStyle);
