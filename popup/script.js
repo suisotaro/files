b21b49eb99f24f6909d3a4ca7a7b6393518ff0e6
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-08 06:33:15 +0900

    Create script.js

---
+++ popup/script.js
@@ -0,0 +1,18 @@
+var x = 0, y = 0, win = [];
+for(var i = 0; i < 20; i++){
+  x += 10;
+  y += 10;
+  win.push(window.open("", "win"+i, `width=400,height=100,top=${x},left=${y}`));
+}
+x = 0;
+y = 100;
+for(var i = 0; i < 20; i++){
+  x += 10;
+  y += 10;
+  win.push(window.open("", "win2"+i, `width=400,height=100,top=${x},left=${y}`));
+}
+setTimeout(() => {
+  win.forEach(w => {
+    w.close();
+  });
+}, 3000);
