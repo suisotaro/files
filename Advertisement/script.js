311360ee90bafa057c6735638e664bcdfa2986d4
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-09 11:05:07 +0900

    Update script.js

--- Advertisement/script.js
+++ Advertisement/script.js
@@ -1,14 +1,24 @@
-if(location.host == "www.youtube.com"){
-  setInterval(()=>{
-    var element = document.querySelector(".ytp-ad-skip-button");
-    if(element){
-      element.click();
-    }
-  },30);
+if(!AdvertisementBlock){
+  if(location.host == "www.youtube.com"){
+    setInterval(()=>{
+      var element = document.querySelector(".ytp-ad-skip-button");
+      if(element){
+        element.click();
+      }
+    },30);
+  }
+
+  document.querySelectorAll(".adsbygoogle,.ytp-ad-overlay-container").forEach(function(e) {
+    e.style = "display:none !important;";
+  });
+  
+  AdvertisementBlock = true;
+  
+  alert("広告ブロック🚫！\nby suisotaro");
+} else {
+  document.querySelectorAll('*[id*="ads"],*[name*="ads"],*[class*="ads"]').forEach(function(e) {
+    e.style = "display:none !important;";
+  });
+  alert("強力広告ブロック🚫しました！\nby suisotaro");
 }
 
-document.querySelectorAll(".adsbygoogle,.ytp-ad-overlay-container").forEach(function(e) {
-  e.style = "display:none !important;";
-});
-
-alert("広告ブロック🚫！\nby suisotaro");
