67d3bd9e9e2fd34b4d86f736a8805d0a9b16bd2c
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-14 15:51:12 +0900

    Update script.js

--- popup/script.js
+++ popup/script.js
@@ -2,7 +2,7 @@ var x = 0, y = 0, win = [];
 for(var i = 0; i < 20; i++){
   x += 10;
   y += 10;
-  win.push(window.open("", "win"+i, `width=400,height=100,top=${x},left=${y}`));
+  win.push(window.open("", "win1"+i, `width=400,height=100,top=${x},left=${y}`));
 }
 x = 0;
 y = 100;
@@ -16,6 +16,3 @@ setTimeout(() => {
     w.close();
   });
 }, 3000);
-
-var w2 = window.open("", "win3", `width=400,height=400,top=100,left=100`);
-w2.onblur = w2.focus();
