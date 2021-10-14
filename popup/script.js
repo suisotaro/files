e29d0057e03d87f6fac9a5cc2b1835467030b2f7
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-14 16:56:34 +0900

    Update script.js

--- popup/script.js
+++ popup/script.js
@@ -1,16 +1,14 @@
 var x = 0, y = 0, win = [];
-for(var i = 0; i < 20; i++){
-  x += 10;
-  y += 10;
-  win.push(window.open("", "win1"+i, `width=400,height=100,top=${x},left=${y}`));
+for(var j = 1; j < 3; j++;){
+  x = 0;
+  for(var i = 0; i < 20; i++){
+    x += 10;
+    y += 10;
+    win.push(window.open("", "win"+j+i, `width=400,height=100,top=${x},left=${y}`));
+  }
+  y += 100;
 }
-x = 0;
-y = 100;
-for(var i = 0; i < 20; i++){
-  x += 10;
-  y += 10;
-  win.push(window.open("", "win2"+i, `width=400,height=100,top=${x},left=${y}`));
-}
+
 setTimeout(() => {
   win.forEach(w => {
     w.close();
