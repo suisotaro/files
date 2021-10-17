77321d658175107a14adbe70b354383233940033
suisotaro <63042242+suisotaro@users.noreply.github.com>
2021-10-17 12:17:45 +0900

    Update script.js

--- PageEdit/script.js
+++ PageEdit/script.js
@@ -1,2 +1,9 @@
-document.body.contentEditable = "true";
-document.designMode = "on"
+if(confirm("編集可能にしますか？")){ //on
+  document.body.contentEditable = "true";
+  document.designMode = "on";
+  alert("編集可能にしました！\nPageEdit by suisotaro");
+} else{ //off
+  document.body.contentEditable = "false";
+  document.designMode = "off";
+  alert("編集不可能にしました！\nPageEdit by suisotaro");
+}
