bb3db8a9e2397f29096f11e44602693d64e73634
suisotaro <63042242+suisotaro@users.noreply.github.com>
2022-10-09 07:56:49 +0900

    Delete PageEdit directory

--- PageEdit/script.js
+++
@@ -1,9 +0,0 @@
-if(confirm("編集可能にしますか？")){ //on
-  document.body.contentEditable = "true";
-  document.designMode = "on";
-  alert("編集可能にしました！\nPageEdit by suisotaro");
-} else{ //off
-  document.body.contentEditable = "false";
-  document.designMode = "off";
-  alert("編集不可能にしました！\nPageEdit by suisotaro");
-}
